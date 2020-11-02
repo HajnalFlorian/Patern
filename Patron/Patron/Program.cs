d30bd2585fc282a3e36475cd204c02a25a88e41b
HajnalFlorian <32631861+HajnalFlorian@users.noreply.github.com>
2020-11-02 14:37:12 +0100

    Patron fabrique et prototype.

---
+++ Patron/Patron/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Patron
+{
+    // patron Fabrique
+    abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+        public string SomeOperation()
+        {
+            var procduct = FactoryMethod();
+
+            var result = "Créateur : Le code du même créateur vient de fonctionner avec" + procduct.Operation();
+
+            return result;
+        }
+    }
+
+    class ConcreteCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+    class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct2();
+        }
+    }
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Resultat de ContreteProduct1}";
+        }
+    }
+    class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Resultat de ContreteProduct2}";
+        }
+    }
+    class client
+    {
+        public void Main()
+        {
+            Console.WriteLine("app: Lancer avec le ConcreteCreator1");
+            ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("app: Lancer avec le ConcreteCreator2");
+            ClientCode(new ConcreteCreator2());
+        }
+        public void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Client : Je ne connais pas la classe du créateur" + ", mais cela fonctionne.\n" + creator.SomeOperation());
+        }
+
+    }
+
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new client().Main();
+        }
+    }
+}
---
+++ Patron_Prototype/Patron_Prototype/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Patron_Prototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime dateNaissance;
+        public string Nom;
+        public IdInfo IdInfo;
+        //copy standar
+        public Person Copy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+        //copy profonde
+        public Person CopyTotal()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Nom = string.Copy(Nom);
+            return clone;
+        }
+    }
+    public class IdInfo
+    {
+        public int IdNumber;
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 30;
+            p1.dateNaissance = Convert.ToDateTime("1990-05-31");
+            p1.Nom = "Billy Kid";
+            p1.IdInfo = new IdInfo(13);
+
+            Person p2 = p1.Copy();
+
+            Person p3 = p1.CopyTotal();
+
+            Console.WriteLine("valeur p1:");
+            affichage(p1);
+            Console.WriteLine("valeur p2:");
+            affichage(p2);
+            Console.WriteLine("valeur p3:");
+            affichage(p3);
+
+            // modification de p1 et réaffichage
+            p1.Age = 45;
+            p1.dateNaissance = Convert.ToDateTime("1975-05-10");
+            p1.Nom=("Jack");
+            p1.IdInfo.IdNumber = 40;
+            Console.WriteLine("\n modfication de p1");
+            Console.WriteLine("valeur p1:");
+            affichage(p1);
+            Console.WriteLine("valeur p2:(id change)");
+            affichage(p2);
+            Console.WriteLine("valeur p3:(copy profonde ne change pas)");
+            affichage(p3);
+
+
+        }
+
+        public static void affichage(Person p)
+        {
+            Console.WriteLine("      Nom: {0:s}, Age: {1:d}, Date de Naissance: {2:MM/dd/yy}",
+                p.Nom, p.Age, p.dateNaissance);
+            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
+        }
+    }
+
+}
+
